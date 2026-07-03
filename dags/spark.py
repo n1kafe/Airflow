6e84a3f99af059c43d3c89fe598a36a92a780701
n0rn552 <nikitaafendi8@gmail.com>
2026-07-03 18:02:05 +0200

    Init commit

---
+++ dags/spark.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from airflow.decorators import dag, task
+
+default_args = {
+    'owner': "airflow",
+    'retries': 1
+}
+
+@dag(
+    dag_id='spark_taskflow_example',
+    default_args=default_args,
+    schedule='@daily',
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['sparl', 'airflow']
+)
+def spark_pipeline():
+    
+    @task
+    def process_data_with_spark():
+        from pyspark.sql import SparkSession
+
+        spark = SparkSession.builder \
+            .appName('AirflowTest') \
+            .master("local[*]") \
+            .config("spark.ui.showConsoleProgress", "false") \
+            .getOrCreate()
+        
+        data = [("Alice", 25), ("Bob", 30), ("Charline", 35)]
+        df = spark.createDataFrame(data, ["Name", "Age"])
+
+        record_count = df.count()
+        print(f"Rows processed: {record_count}")
+
+        spark.stop()
+
+        return record_count
+    
+    process_data_with_spark()
+
+
+spark_pipeline()
